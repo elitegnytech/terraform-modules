7ba6d9b5da79d32da709566a36996079a22c9232
TrungRueta <trungrueta@icloud.com>
2024-06-18 19:25:16 +0700

    update

--- modules/gw-api-post-lambda/main.tf
+++ modules/gw-api-post-lambda/main.tf
@@ -78,6 +78,7 @@ resource "aws_api_gateway_method_response" "this" {
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [
     aws_api_gateway_integration.this,
+    aws_api_gateway_integration_response.this
   ]
 
   rest_api_id       = var.rest_api_id
