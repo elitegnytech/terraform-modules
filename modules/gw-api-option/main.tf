3e39fad8d9009038eec215c3f86a37cb67a8162f
TrungRueta <trungrueta@icloud.com>
2024-06-18 19:02:07 +0700

    add : gateway api option

---
+++ modules/gw-api-option/main.tf
@@ -0,0 +1,107 @@
+variable "rest_api_id" {
+  type = string
+}
+
+variable "root_resource_id" {
+  type = string
+}
+
+variable "path" {
+  type = string
+}
+
+variable "stage_name" {
+  type = string
+}
+
+variable "stage_description" {
+  type = string
+}
+
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+
+resource "aws_api_gateway_resource" "api" {
+  rest_api_id = var.rest_api_id
+  parent_id   = var.root_resource_id
+  path_part   = var.path
+}
+
+# setup options route
+
+resource "aws_api_gateway_method" "this" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = aws_api_gateway_resource.api.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id          = var.rest_api_id
+  resource_id          = aws_api_gateway_resource.api.id
+  http_method          = aws_api_gateway_method.this.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+
+  request_templates = {
+    "application/json" = jsonencode(
+      {
+        statusCode = 200
+      }
+    )
+  }
+}
+
+resource "aws_api_gateway_integration_response" "this" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.this.http_method
+  status_code = aws_api_gateway_method_response.this.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.this,
+    aws_api_gateway_integration.this
+  ]
+}
+
+resource "aws_api_gateway_method_response" "this" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.this.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_deployment" "this" {
+  depends_on = [
+    aws_api_gateway_integration.this,
+  ]
+
+  rest_api_id       = var.rest_api_id
+  stage_name        = var.stage_name
+  stage_description = var.stage_description
+}
