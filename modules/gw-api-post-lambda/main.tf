fbacbe8f5b0445dcb00c5ca75f0e199ac77b6eca
TrungRueta <trungrueta@icloud.com>
2024-06-18 19:30:39 +0700

    update

--- modules/gw-api-post-lambda/main.tf
+++ modules/gw-api-post-lambda/main.tf
@@ -83,6 +83,6 @@ resource "aws_api_gateway_deployment" "this" {
 
   rest_api_id       = var.rest_api_id
   stage_name        = var.stage_name
-  stage_description = var.stage_description
+  stage_description = "${var.stage_description} - ${aws_api_gateway_integration.this.id}"
   description = "Created by terraform - ${aws_api_gateway_integration.this.id}"
 }
